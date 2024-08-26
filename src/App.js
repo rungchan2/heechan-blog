6f0d88093d2203dad9c8f7ca2954e9791d8b1701
Heechan <heechan@Heechanui-MacBookPro.local>
2024-08-26 10:08:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>희찬 테스트 업로드 입니다.</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        블로그를 만들어서 업로드하려다가 시간이 부족해서 이렇게 리드미 파일로 대신합니다...
         </p>
         <a
           className="App-link"
